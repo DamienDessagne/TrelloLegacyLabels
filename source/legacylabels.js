dc1dd262d2ba3c604a2f9cb529bf2de98d6d7024
Damien Dessagne <damien@embers.games>
2022-09-24 19:17:15 +0200

    Firefox working extension v1.0

---
+++ source/legacylabels.js
@@ -0,0 +1,33 @@
+// Hide the little dot on labels :
+var style = document.createElement("style")
+document.head.appendChild(style);
+style.sheet.insertRule("button::before { width:0px!important; }", style.sheet.cssRules.length);
+
+// Observe DOM mutations and apply modifications on the fly :
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+var observer = new MutationObserver(function(mutations, observer) {
+	var labelContainers = document.getElementsByClassName("list-card-front-labels-container");
+	if(labelContainers.length == 0)
+		return;
+	
+	var s = "";
+	for(var i = 0 ; i < labelContainers.length ; i++) {
+		var buttons = labelContainers[i].getElementsByTagName("button");
+		if(buttons != null && buttons.length > 0) {
+			for(var j = 0 ; j < buttons.length ; j++) {
+				var b = buttons[j];
+				b.style.color = b.getAttribute("data-expanded") == "false" ? "transparent" : "#ffffff";
+				var dot = window.getComputedStyle(b,':before');
+				b.style.fontWeight = "bold";
+				b.style.backgroundColor = dot.backgroundColor;
+				b.style.paddingLeft = "8px";
+			}
+		}
+	}
+});
+
+observer.observe(document.body, {
+	subtree: true,
+	attributes: true
+});
