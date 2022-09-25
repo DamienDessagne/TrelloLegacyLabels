4525cff85285bc85aadf689e68bb328c28396d7d
Damien Dessagne <damien@embers.games>
2022-09-25 11:32:52 +0200

    Version 1.1, compatible with Firefox and Chrome

---
+++ build.ps1
@@ -0,0 +1,15 @@
+﻿$MyInvocation.MyCommand.Path | Split-Path | Push-Location
+
+Push-Location -Path ".\source"
+
+# V2 version
+(Get-Content manifest.json) -Replace '"manifest_version": 3,', '"manifest_version": 2,' | Set-Content manifest.json
+Compress-Archive -DestinationPath "..\dist\TrelloLegcyLabels.v2.zip" -CompressionLevel Optimal -Path ".\legacylabels.js", ".\icons", ".\manifest.json" -Force
+
+# V3 version
+(Get-Content manifest.json) -Replace '"manifest_version": 2,', '"manifest_version": 3,' | Set-Content manifest.json
+Compress-Archive -DestinationPath "..\dist\TrelloLegcyLabels.v3.zip" -CompressionLevel Optimal -Path ".\legacylabels.js", ".\icons", ".\manifest.json" -Force
+
+# Clean up
+Pop-Location
+Pop-Location
--- source/legacylabels.js
+++ source/legacylabels.js
@@ -1,7 +1,7 @@
 // Hide the little dot on labels :
 var style = document.createElement("style")
 document.head.appendChild(style);
-style.sheet.insertRule("button::before { width:0px!important; }", style.sheet.cssRules.length);
+style.sheet.insertRule("button.legacy-label::before { width:0px!important; }", style.sheet.cssRules.length);
 
 // Observe DOM mutations and apply modifications on the fly :
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
@@ -22,6 +22,8 @@ var observer = new MutationObserver(function(mutations, observer) {
 				b.style.fontWeight = "bold";
 				b.style.backgroundColor = dot.backgroundColor;
 				b.style.paddingLeft = "8px";
+				if(!b.classList.contains("legacy-label"))
+					b.classList.add("legacy-label");
 			}
 		}
 	}
