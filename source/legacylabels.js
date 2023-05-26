a716e46ecc6cd26e2f6d5ccfb7fa0f074a1db31b
Damien Dessagne <damien@embers.games>
2023-05-26 18:32:59 +0200

    Improved performance for boards with loads of labels

--- source/legacylabels.js
+++ source/legacylabels.js
@@ -1,4 +1,33 @@
-﻿var isUpdating = false;
+﻿MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+var hasDotCSS = false;
+var isUpdating = false;
+
+updateLabelOnChange = function(mutations, observer) {
+	var labelContainers = document.getElementsByClassName("list-card-front-labels-container");
+	if(labelContainers.length == 0)
+		return;
+	
+	var s = "";
+	for(var i = 0 ; i < labelContainers.length ; i++) {
+		var buttons = labelContainers[i].getElementsByTagName("button");
+		if(buttons != null && buttons.length > 0) {
+			for(var j = 0 ; j < buttons.length ; j++) {
+				var b = buttons[j];
+				if(b.getAttribute("data-expanded") == "false") {
+					b.style.color = "transparent";
+				}
+				else {
+					var dot = window.getComputedStyle(b,':before');
+					b.style.backgroundColor = dot.backgroundColor;
+					if(!b.classList.contains("legacy-label"))
+						b.classList.add("legacy-label");
+				}
+			}
+		}
+	}
+	isUpdating = false;
+};
+
 
 updateLabels = function() {
 	if(isUpdating) // -> update already in progress
@@ -7,42 +36,16 @@ updateLabels = function() {
 	isUpdating = true;
 	
 	// Hide the little dot on labels :
-	var style = document.createElement("style")
-	document.head.appendChild(style);
-	style.sheet.insertRule("button.legacy-label::before { width:0px!important; }", style.sheet.cssRules.length);
+	if(!hasDotCSS) {
+		var style = document.createElement("style")
+		document.head.appendChild(style);
+		style.sheet.insertRule("button.legacy-label::before { width:0px!important; }", style.sheet.cssRules.length);
+		style.sheet.insertRule("button.legacy-label { color:#ffffff; font-weight:bold; padding-left:8px;}", style.sheet.cssRules.length);
+		hasDotCSS = true;
+	}
 
 	// Observe DOM mutations and apply modifications on the fly :
-	MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-
-	observer = new MutationObserver(function(mutations, observer) {
-		var labelContainers = document.getElementsByClassName("list-card-front-labels-container");
-		if(labelContainers.length == 0)
-			return;
-		
-		var s = "";
-		for(var i = 0 ; i < labelContainers.length ; i++) {
-			var buttons = labelContainers[i].getElementsByTagName("button");
-			if(buttons != null && buttons.length > 0) {
-				for(var j = 0 ; j < buttons.length ; j++) {
-					var b = buttons[j];
-					if(b.getAttribute("data-expanded") == "false") {
-						b.style.color = "transparent";
-					}
-					else {
-						b.style.color = "#ffffff";
-						var dot = window.getComputedStyle(b,':before');
-						b.style.fontWeight = "bold";
-						b.style.backgroundColor = dot.backgroundColor;
-						b.style.paddingLeft = "8px";
-						if(!b.classList.contains("legacy-label"))
-							b.classList.add("legacy-label");
-					}
-				}
-			}
-		}
-		isUpdating = false;
-	});
-
+	observer = new MutationObserver(updateLabelOnChange);
 	observer.observe(document.body, {
 		subtree: true,
 		attributes: true
@@ -62,6 +65,7 @@ setInterval(
 			return;
 		
 		currentPath = window.location.pathname;
+		hasDotCSS = false;
 		if(currentPath.startsWith('/b/'))
 			updateLabels();
 		
